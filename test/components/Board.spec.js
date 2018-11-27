18444bf113be71097f9681d679af333e61e54c7c
Gabriel Wu <qqbbnease1004@126.com>
2018-11-27 21:40:09 +0800

    test: make all tests pass

--- test/components/Board.spec.js
+++ test/components/Board.spec.js
@@ -3,26 +3,29 @@ import chai from 'chai'
 import Board from '../../src/components/Board'
 chai.should()
 
+// Configure JSDOM
 const {JSDOM} = jsdom
 const dom = new JSDOM(`<!DOCTYPE html><body><div><canvas id="test" width="800" height="600"></canvas></div></body>`, {
+  url: 'https://example.org/',
   pretendToBeVisual: true,
 })
-const {document} = dom.window
-global.document = document
+
+// Configure global variables
 global.window = dom.window
+global.document = window.document
+global.HTMLElement = window.HTMLElement
+global.Element = window.Element
+global.localStorage = window.localStorage
 
 const {fabric} = require('../../lib/fabric.min')
 global.fabric = fabric
 
+// Start tests
 describe('Board', () => {
   const canvas = document.getElementById('test')
   const board = new Board(canvas)
 
-  afterEach(() => {
-    board.clear()
-  })
-
-  it('can draw a line', () => {
+  beforeEach(() => {
     board.layerDraw.add(
       new fabric.Line([50, 100, 200, 200], {
         left: 170,
@@ -30,6 +33,25 @@ describe('Board', () => {
         stroke: 'red',
       }),
     )
+  })
+
+  afterEach(() => {
+    board.clear()
+  })
+
+  it('can draw a line', () => {
+    board.layerDraw._objects[0].aCoords.tl.x.should.equal(170)
+  })
+
+  it('can be cleared', () => {
+    board.clear()
+    board.layerDraw._objects.length.should.equal(0)
+  })
+
+  it('can be saved and then loaded', () => {
+    board.save()
+    board.clear()
+    board.load()
     board.layerDraw._objects[0].aCoords.tl.x.should.equal(170)
   })
 })
