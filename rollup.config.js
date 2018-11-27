24679e788be2b8ffd992e87bf8125f5a24280673
Gabriel Wu <qqbbnease1004@126.com>
2018-11-27 11:46:58 +0800

    feat: uglify bundled packages with rollup-plugin-uglify and rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
-// import json from 'rollup-plugin-json'
-// import babel from 'rollup-plugin-babel'
-// import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
 // import commonjs from 'rollup-plugin-commonjs'
+// import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve'
+import {terser} from 'rollup-plugin-terser'
+import {uglify} from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
+const defaultPlugins = [
+  resolve(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+]
+
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/index.js',
     output: {
@@ -13,40 +21,25 @@ export default [
       file: pkg.browser,
       format: 'umd',
     },
-    // plugins: [
-    //   resolve({
-    //     jsnext: true,
-    //     main: true,
-    //     preferBuiltins: false,
-    //   }), // so Rollup can find `ms`
-    //   babel({
-    //     exclude: 'node_modules/**', // compile our source code only
-    //   }),
-    //   commonjs(), // so Rollup can convert `ms` to an ES module
-    //   json(),
-    // ],
+    plugins: [...defaultPlugins, uglify()],
+  },
+  // FIXME: es and cjs outputs should be merged
+  // They are splitted for now as a workaround
+  // for rollup-plugin-terser.
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.module,
+      format: 'es',
+    },
+    plugins: [...defaultPlugins, terser()],
   },
   {
     input: 'src/index.js',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-      },
-    ],
-    // plugins: [resolve({
-    //     jsnext: true,
-    //     main: true,
-    //   }),
-    //   babel({
-    //     exclude: 'node_modules/**',
-    //   }),
-    //   commonjs(),
-    //   json(),
-    // ]
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    plugins: [...defaultPlugins, terser()],
   },
 ]
